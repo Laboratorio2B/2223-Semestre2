82fa337f805cd15d031846d6a332db6e0dd37007
Laboratorio2B <giovanni.manzini@unipi.it>
2023-02-13 11:44:06 +0100

    aggiunte pattern rule nel makefile

--- pipe/npipe.c
+++ pipe/npipe.c
@@ -8,38 +8,25 @@ int main(int argc, char *argv[])
     exit(1);
   }
   
-  // crea la named pipe da usare per le comunicazioni
-  int e = mkfifo(argv[1],0666);
-  if(e==0)
-    puts("Named pipe creata");
-  else if(e== EEXIST)
-    puts("La named pipe esiste già; procedo...");
-  else    
-    termina("Errore creazione named pipe");
-
-  // faccio partire il lettore
-  //if(xfork(__LINE__,__FILE__)==0) {
-  //  if(execl("lettore", "lettore", argv[1], (char *) NULL)==-1)
-  //    termina("exec fallita");
-  //}
-  
   // crea processi scrittori ausiliari
   int n=2; 
   for(int i=0;i<n;i++) {
     pid_t p = xfork(__LINE__,__FILE__);
     if(p==0) {// figlio
       // apre file descriptor associato alla named pipe
-      // se il file non esiste termina con errore  
-      int fd = open(argv[1],O_WRONLY);
-      if(fd<0) termina("Errore apertura named pipe");
-      for(int j=0; ;j++) {
-        // il filio-esimo scrive interi =i mod n 
+      int fd = open(argv[1], O_WRONLY);
+      if (fd < 0) // se il file non esiste termina con errore
+        xtermina("Errore apertura named pipe", __LINE__, __FILE__);
+      // scrive interi sulla pipe per sempre
+      for (int j = 0;; j++)
+      {
+        // il figlio-esimo scrive interi =i mod n 
         int val = j*n + i;
         ssize_t e = write(fd,&val,sizeof(val));
         if(e!=sizeof(val))
-          termina("Errore scrittura pipe");
+          xtermina("Errore scrittura pipe",__LINE__,__FILE__);
         if(j%10000==0)
-           printf("%d (figlio di %d): scritti %d interi\n",getpid(),getppid(),j);
+          printf("%d (figlio di %d): scritti %d interi\n",getpid(),getppid(),j);
       }
       printf("Io figlio %d ho finito.\n",getpid());
       exit(0);
@@ -49,3 +36,19 @@ int main(int argc, char *argv[])
   printf("Io %d ho finito.\n",getpid());
   return 0;
 }
+
+  // // crea la named pipe da usare per le comunicazioni
+  // int e = mkfifo(argv[1],0666);
+  // if(e==0)
+  //   puts("Named pipe creata");
+  // else if(errno== EEXIST)
+  //   puts("La named pipe esiste già; procedo...");
+  // else    
+  //   xtermina("Errore creazione named pipe",__LINE__,__FILE__);
+
+
+  // faccio partire il lettore
+  //if(xfork(__LINE__,__FILE__)==0) {
+  //  if(execl("lettore", "lettore", argv[1], (char *) NULL)==-1)
+  //    termina("exec fallita");
+  //}
