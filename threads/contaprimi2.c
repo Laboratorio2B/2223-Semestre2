be8f5a72c76dbe7ddc5830a96b32687655f2e187
Laboratorio2B <giovanni.manzini@unipi.it>
2023-02-28 13:05:42 +0100

    lezione sui thread

--- threads/contaprimi2.c
+++ threads/contaprimi2.c
@@ -18,29 +18,20 @@ bool primo(int n);
 // struct che uso per passare argomenti ai thread
 typedef struct {
 	int start;   // intervallo dove cercare i primo 
-	int end;
-  sem_t *a0;   // puntatore al semaforo a0x
-  sem_t *finito;  // puntatore al semaforo finitox
-  int *somma;  // puntatore alla somma condivisa 
+	int end;     // parametri di input
+    int somma_parziale; // parametro di output
 } dati;
 
 // funzione passata a pthred_create
 void *tbody(void *v) {
 	dati *d = (dati *) v;
+    int somma = 0;
 	// cerco i primi nell'intervallo assegnato
-	for(int j=d->start;j<d->end;j++) {
-    if(primo(j)) {
-          // aspetta che il semaforo sia 1
-					xsem_wait(d->a0,__LINE__, __FILE__);
-          *(d->somma) += 1;
-          // riporta il semaforo a 1
-					xsem_post(d->a0,__LINE__, __FILE__);	
-    }
-	}
-  fprintf(stderr,"Il thread che partiva da %d ha terminato\n",d->start);
-  // segnala al processo padre che questo processo ha finito 
-	xsem_post(d->finito,__LINE__, __FILE__);
-  pthread_exit(NULL);
+	for(int j=d->start;j<d->end;j++)
+      if(primo(j)) somma++;
+    fprintf(stderr,"Il thread che partiva da %d ha terminato\n",d->start);
+    d->somma_parziale = somma;
+    pthread_exit(NULL);
 }
 
 
@@ -56,32 +47,24 @@ int main(int argc,char *argv[])
   int p= atoi(argv[2]);
   if(p<=0) termina("numero di thread non valido");
 
-  // ---- creo i semafori (senza nome)
-	sem_t sem_a0x, sem_finitox;
-  // inizalizzo sem_a0x e sem_finitox
-	xsem_init(&sem_a0x,0,1,__LINE__, __FILE__);
-	xsem_init(&sem_finitox,0,0,__LINE__, __FILE__);
-
   // creazione thread ausiliari
   pthread_t t[p];   // array di p indentificatori di thread 
-	dati d[p];        // array di p struct che passerò allle p thread
-	int somma = 0;        // variabile dove accumulo il numero di primi
+  dati d[p];        // array di p struct che passerò allle p thread
+  int somma = 0;        // variabile dove accumulo il numero di primi
   for(int i=0; i<p; i++) {
     int n = m/p;  // quanti numeri verifica ogni figlio + o - 
     d[i].start = n*i; // inizio range figlio i
     d[i].end = (i==p-1) ? m : n*(i+1);
-		d[i].a0 = &sem_a0x;
-		d[i].finito = &sem_finitox;
-		d[i].somma = &somma;
     xpthread_create(&t[i], NULL, &tbody, &d[i],__LINE__, __FILE__); 
   }
-  // aspetta che abbiano finito tutti i figli: 
-  for(int i=0; i<p; i++) 
-		xsem_wait(&sem_finitox,__LINE__, __FILE__);
-    
+  // attendo che i thread abbiano finito
+  for(int i=0;i<p;i++) {
+    xpthread_join(t[i],NULL,__LINE__, __FILE__);
+    somma += d[i].somma_parziale;
+  }
   // restituisce il risultato 
   printf("Numero primi tra 1 e %d (escluso): %d\n",m,somma);
-	return 0;
+  return 0;
 }
 
 
--- threads/xerrori.h
+++ threads/xerrori.h
@@ -61,5 +61,3 @@ int xpthread_mutex_init(pthread_mutex_t *restrict mutex, const pthread_mutexattr
 int xpthread_mutex_destroy(pthread_mutex_t *mutex, int linea, char *file);
 int xpthread_mutex_lock(pthread_mutex_t *mutex, int linea, char *file);
 int xpthread_mutex_unlock(pthread_mutex_t *mutex, int linea, char *file);
-
-
